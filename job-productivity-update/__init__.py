7de2e29f79f6dc954e8303547227589882df5414
Edward Russell <newunit13@gmail.com>
2021-12-16 13:25:44 -0500

    added functionality

--- job-productivity-update/__init__.py
+++ job-productivity-update/__init__.py
@@ -13,54 +13,57 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
 
-    # TODO: Run job profitability report to get all jobs that have no been invoiced. Include [Actual Start Date]
+    # Run job profitability report to get all jobs that have no been invoiced. Include [Actual Start Date]
     report_code = "job_profitability"
     columns = ["actual_job_start_at"]
     filters = [{"column": "invoiced", "operator": "=", "threshold": "false"}]
 
     report = nu.get_report(report_code, columns, filters, headers=False)
 
-    data = []
+    jobs_not_invoiced = {}
     for line in report:
-        if line[1]:
-            data.append(
-                (line[0], datetime.datetime.strptime(line[1], "%Y-%b-%d %I:%M %p").date())
-            )
+        if not line[1]:
+            continue
 
-    # TODO: Run job productivity report filtered to min/max start date from job profitability report.
+        jobs_not_invoiced[line[0]] = datetime.datetime.strptime(line[1], "%Y-%b-%d %I:%M %p").date()
+
+    # Run job productivity report filtered to min/max start date from job profitability report.
+
+    earliest_job = min(jobs_not_invoiced.values()).strftime('%Y-%b-%d 00:00 AM')
+    latest_job = max(jobs_not_invoiced.values()).strftime('%Y-%b-%d 11:59 PM')
 
     report_code = "job_productivity"
-    columns = ["line_name", "line_leader_name", "units_produced", "units_remaining", "unit_of_measure", "performance", "availability", "line_efficiency", "percent_complete"]
-    filters = [{"column": "actual_job_start_at", "operator": "between", "from_threshold": min(), "to_threshold": "" }]
+    columns = ["line_name", "line_leader_name", "actual_job_end_at", "project_customer", "item_code", "units_expected", "units_produced", "pallets_produced", "standard_people", "number_of_personnel", "actual_person_hours", "line_efficiency"]
+    filters = [{"column": "actual_job_start_at", "operator": "between", "from_threshold": earliest_job, "to_threshold": latest_job }]
     sort_by = [{"column": "line_name", "direction": 'asc'}]
 
-    
+
     report = nu.get_report(report_code=report_code, columns=columns, filters=filters, sort_by=sort_by, headers=False)
 
-    data = []
+    production_records = {}
     for line in report:
-        data.append({
-            "line_name"         : line[1],
-            "line_leader"       : line[2],
-            "units_produced"    : float(line[3]),
-            "units_remaining"   : float(line[4]) if float(line[4]) >= 0 else 0,
-            "unit_of_measure"   : line[5],
-            "line_performance"  : float(line[6].replace('%','')),
-            "line_availability" : float(line[7].replace('%','')),
-            "line_effeciency"   : float(line[8].replace('%','')),
-            "timestamp"         : datetime.now().strftime("%m/%d/%Y %H:%M:%S"),
-            "percent_complete"  : f"{float(line[9].replace('%','')):0.0f}%"
-        })
+        production_records[line[0]] = {
+            "Job ID"                : line[0],
+            "Line Name"             : line[1],
+            "Line Leader"           : line[2],
+            "Actual Job End Date"   : line[3],
+            "Work Order Customer"   : line[4],
+            "Item Code"             : line[5],
+            "Units Expected"        : float(line[6]),
+            "Units Produced"        : float(line[7]),
+            "Pallets Produced"      : float(line[8]),
+            "Standard People"       : float(line[9]),
+            "Actual People"         : float(line[10]),
+            "Actual Hours"          : float(line[11]),
+            "Line Efficiency"       : line[12]
+        }
 
-    sql.insert(table="job_productivity", record=data)
+    # Match jobs from job productivity report to jobs from job profitability report.
+    production_records = {k:v for k,v in production_records.items() if k in jobs_not_invoiced}
 
-    # TODO: Match jobs from job productivity report to jobs from job profitability report.
-    
-    # TODO: Check database for job ids that are in dataset.
-    # if job_id in dataset:
-    #   update recod
-    # else:
-    #  insert record 
+    # Check database for job ids that are in dataset.
+    for job_id, details in production_records.items():
 
+        sql.insert_or_update('PRODUCTION_RECORDS', 'Job ID', details)
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
